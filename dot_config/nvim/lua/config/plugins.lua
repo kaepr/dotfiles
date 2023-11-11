8ee1803cdd53a6ff598ef0b1ab10ea90cc30d4b2
Shagun Agrawal <agrawalshagun07@gmail.com>
2023-11-11 10:50:25 +0530

    nvim changes

--- dot_config/nvim/lua/config/plugins.lua
+++ dot_config/nvim/lua/config/plugins.lua
@@ -17,48 +17,41 @@ local plugins = {
         "rebelot/kanagawa.nvim",
         config = function()
             require('kanagawa').setup({
-                compile = false, -- enable compiling the colorscheme
+                compile = true,   -- enable compiling the colorscheme
                 undercurl = true, -- enable undercurls
                 commentStyle = { italic = false },
-                functionStyle = {},
+                functionStyle = { italic = false },
                 keywordStyle = { italic = false },
-                statementStyle = { bold = true },
-                typeStyle = {},
-                transparent = false, -- do not set background color
-                dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+                statementStyle = {
+                    bold = true,
+                    italic = false
+                },
+                typeStyle = {
+                    italic = false
+                },
+                transparent = false,   -- do not set background color
+                dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
                 terminalColors = true, -- define vim.g.terminal_color_{0,17}
-                colors = { -- add/modify theme and palette colors
+                colors = {             -- add/modify theme and palette colors
                     palette = {},
                     theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
                 },
                 overrides = function(colors) -- add/modify highlights
-                    return {}
+                    return {
+                        -- Assign a static color to strings
+                        String = { fg = colors.palette.carpYellow, italic = false },
+                        -- theme colors will update dynamically when you change theme!
+                        SomePluginHl = { fg = colors.theme.syn.type, bold = true },
+                        Italic = { italic = false },
+                        Special = { italic = false },
+                    }
                 end,
-                theme = "dragon", -- Load "wave" theme when 'background' option is not set
-                background = { -- map the value of 'background' option to a theme
+                theme = "dragon",    -- Load "wave" theme when 'background' option is not set
+                background = {       -- map the value of 'background' option to a theme
                     dark = "dragon", -- try "dragon" !
                     light = "lotus"
                 },
             })
-        end
-    },
-
-    {
-        "ellisonleao/gruvbox.nvim",
-        priority = 1000,
-        config = function()
-            require("gruvbox").setup({
-                constrast = "hard",
-                italic = {
-                    strings = false,
-                    comments = false,
-                    operators = false,
-                    folds = false,
-                },
-            })
-
-            -- vim.o.background = "light"
-            -- vim.cmd("colorscheme gruvbox")
         end
     },
 
